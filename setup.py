a8552cfbbb3d553cbd96a82294bb5129864b3008
Jonatan Heyman <jonatan@heyman.info>
2018-01-24 16:06:02 +0100

    Bumped version to upload new PyPI package

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ if sys.hexversion < 0x02070900:
 
 
 setup(name='geventhttpclient-wheels',
-      version = '1.3.1dev',
+      version = '1.3.1dev1',
       description = 'Pre-built wheels for geventhttpclient',
       long_description = DESC,
       url="http://github.com/gwik/geventhttpclient",
