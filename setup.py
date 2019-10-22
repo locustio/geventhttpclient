4d83606a717def7466e79f73d020af737c4b26cf
Jonatan Heyman <jonatan@heyman.info>
2019-10-22 16:26:58 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ if sys.hexversion < 0x02070900:
     ]
 
 setup(name='geventhttpclient-wheels',
-      version = '1.3.1dev1',
+      version = '1.3.1dev2',
       description = 'Pre-built wheels for geventhttpclient',
       long_description = DESC,
       url="http://github.com/gwik/geventhttpclient",
